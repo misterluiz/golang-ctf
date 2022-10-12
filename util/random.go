0d2dba71c866418694bd0878689a0453682b0695
Gabriel Luiz <misterluizpro@gmail.com>
2022-10-12 02:52:25 -0300

    feat: Random Util

---
+++ util/random.go
@@ -0,0 +1,28 @@
+package util
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+func RandomString(number int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+	for i := 0; i < number; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+
+	}
+	return sb.String()
+}
+
+func RandomEmail(number int) string {
+	return fmt.Sprintf("%s@email.com", RandomString(number))
+}
