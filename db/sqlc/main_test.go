7a41764c35bb24b889bd7994a58cc0b6965f7dc2
Gabriel Luiz <misterluizpro@gmail.com>
2022-10-12 02:56:25 -0300

    test: Teste main

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:password@localhost:5432/go-ctf?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("nÃO FOI POSsivel conectar", err)
+	}
+	testQueries = New(conn)
+	os.Exit(m.Run())
+}
