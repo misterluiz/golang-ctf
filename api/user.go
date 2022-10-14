3b7d892efb54293d927996992b36446fa5aed711
Gabriel Luiz <misterluizpro@gmail.com>
2022-10-13 23:12:44 -0300

    feat: add user api

---
+++ api/user.go
@@ -0,0 +1,85 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/misterluiz/golang-ctf/db/sqlc"
+)
+
+type CreateUserRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req CreateUserRequest
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	arg := db.CreateUserParams{
+		Username: req.Username,
+		Password: req.Password,
+		Email:    req.Email,
+	}
+	user, err := server.store.CreateUser(ctx, arg)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+	ctx.JSON(http.StatusOK, user)
+
+}
+
+type getUserRequest struct {
+	Username string `uri:"username" binding:"required"`
+}
+
+func (server *Server) getUser(ctx *gin.Context) {
+	var req getUserRequest
+
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, user)
+}
+
+type getUserByIdRequest struct {
+	ID int32 `uri:"id" binding:"required"`
+}
+
+func (server *Server) getUserById(ctx *gin.Context) {
+	var req getUserByIdRequest
+
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	user, err := server.store.GetUserById(ctx, req.ID)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+
+}
