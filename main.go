a19f080826a47817e86f455ed981bf8099942c24
Gabriel Luiz <misterluizpro@gmail.com>
2022-10-15 18:20:50 -0300

    feat: add variaveis de ambiente

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/misterluiz/golang-ctf/api"
 	db "github.com/misterluiz/golang-ctf/db/sqlc"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:password@localhost:5432/go-ctf?sslmode=disable"
-	serverAddress = "0.0.0.0:8000"
-)
-
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+	serverAddress := os.Getenv("SERVER_ADDRESS")
+
 	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("NÃO FOI POSsivel conectar", err)
