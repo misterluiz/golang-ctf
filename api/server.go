9df435e1e633e8946eee8ec1ef21b14a7b65801a
Gabriel Luiz <misterluizpro@gmail.com>
2022-10-15 18:19:44 -0300

    refactore: add new routes api

--- api/server.go
+++ api/server.go
@@ -15,9 +15,26 @@ func NewServer(store *db.SQLStore) *Server {
 	router := gin.Default()
 
 	//colocar as rotas aqui
+	//rotas user
 	router.POST("/user", server.createUser)
 	router.GET("/user/:username", server.getUser)
 	router.GET("/user/id/:id", server.getUserById)
+
+	//rotas categories
+	router.POST("/category", server.createCategory)
+	router.GET("/category/id/:id", server.getCategory)
+	router.DELETE("/category/:id", server.deleteCategory)
+	router.PUT("/category/:id", server.updateCategory)
+	router.GET("/category", server.getCategories)
+
+	//rotas account
+	router.POST("/account", server.createAccount)
+	router.GET("/account/id/:id", server.getAccount)
+	router.DELETE("/account/:id", server.deleteAccount)
+	router.PUT("/account/:id", server.updateAccount)
+	router.GET("/account", server.getAccounts)
+	router.GET("/account/graph/:user_id/:type", server.getAccountGraph)
+	router.GET("/account/reports/:user_id/:type", server.getAccountReports)
 	server.router = router
 
 	return server
